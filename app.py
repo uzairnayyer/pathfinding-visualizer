a791dbe422e76a9854a60b008c4e5a8afd5185b1
Uzair Nayyer <uzairnayyerw@gmail.com>
2026-01-28 23:34:44 +0500

    Change main block to run app on all interfaces

--- app.py
+++ app.py
@@ -218,5 +218,5 @@ def generate():
     state = grid.generate_maze(density)
     return jsonify(state)
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
